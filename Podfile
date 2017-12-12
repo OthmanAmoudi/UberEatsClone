145f89a6ef6b72d3fded91d5ecdedec6b78908f5
OthmanAmoudi <red.fantastic@hotmail.com>
2017-12-12 23:24:51 +0300

    Added the TrackOrder VC, installed dependencies

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'UberEats' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for UberEats
+	pod 'Stripe'
+	pod 'Firebase'
+
+end
--- UberEats/TrackOrderViewController.swift
+++ UberEats/TrackOrderViewController.swift
@@ -6,4 +6,37 @@
 //  Copyright © 2017 Othman Mashaab. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import MapKit
+
+class TrackOrderViewController:UIViewController {
+    
+    @IBOutlet weak var mapView : MKMapView!
+    @IBOutlet weak var containerView : UIView!
+    @IBOutlet weak var progressView : UIView!
+    @IBOutlet weak var restaurantLabel : UILabel!
+    @IBOutlet weak var statusLabel : UILabel!
+    @IBOutlet weak var tableView : UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        
+    }
+
+}
+
+extension TrackOrderViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TrackItemCell", for: indexPath)
+        return cell
+        
+    }
+}
